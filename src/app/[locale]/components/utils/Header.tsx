c77eb228b36742fe8551e35f69ff8f88fa274476
thanhtung-2k3 <tt8993499@gmail.com>
2025-03-11 16:12:50 +0700

    edit LangSwitcher to keep params in link when change language

--- src/app/[locale]/components/utils/Header.tsx
+++ src/app/[locale]/components/utils/Header.tsx
@@ -161,13 +161,13 @@ export const Header: FC<Props> = ({ locale }) => {
             {t('Chatbot')}
           </Link>
           <Link lang={locale} href={`/chatbot2`} style={{ fontWeight: 'bold' }}>
-            Chatbot2
+            {t('Chatbot2')}
           </Link>
           <Link lang={locale} href={`/support`} style={{ fontWeight: 'bold' }}>
             {t('Support')}
           </Link>
           <Link lang={locale} href={`/addconference`} style={{ fontWeight: 'bold' }}>
-            Add Conference
+            {t('Add Conference')}
           </Link>
           <Link lang={locale} href={`/about`} style={{ fontWeight: 'bold' }}>
             {t('About')}
