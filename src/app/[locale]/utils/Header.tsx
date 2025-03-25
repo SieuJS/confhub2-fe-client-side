67f830a6a1a540980bd60e679020884fa9cc70c9
thanhtung-2k3 <tt8993499@gmail.com>
2025-03-25 15:26:26 +0700

    re-add LoadingIndicator

--- src/app/[locale]/utils/Header.tsx
+++ src/app/[locale]/utils/Header.tsx
@@ -83,6 +83,7 @@ export const Header: FC<Props> = ({ locale }) => {
       <div className='relative flex flex-row items-center gap-4'>
         <DesktopNavigation locale={locale} />
 
+        {isLoading ? <LoadingIndicator/> : 
           <AuthButtons
             isLogin={isLoggedIn} 
             locale={locale}
@@ -90,7 +91,9 @@ export const Header: FC<Props> = ({ locale }) => {
             toggleUserDropdown={() => openUserDropdown()}
             notificationEffect={notificationEffect}
             unreadCount={unreadCount()}
-          />
+        />
+        }
+          
         {/* } */}
 
         {/* Mobile Menu Button */}
