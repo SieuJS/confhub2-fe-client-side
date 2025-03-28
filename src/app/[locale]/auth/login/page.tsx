195852a25d4f0688ebfea21f97ca42860fd23337
thanhtung-2k3 <tt8993499@gmail.com>
2025-03-28 20:17:19 +0700

    fix error still go to LoginPage when access login link while already login

--- src/app/[locale]/auth/login/page.tsx
+++ src/app/[locale]/auth/login/page.tsx
@@ -1,12 +1,34 @@
 "use client";
 
 import LoginForm from "./LoginForm";
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import useAuthApi from '@/src/hooks/auth/useAuthApi'; // Import useAuthApi
 
-function LoginPage() {
+const LoginPage= ({
+  params: { locale }
+}: {
+  params: { locale: string }
+}) => {
+  const { isLoggedIn, isLoading } = useAuthApi(); // Get isLoading
+  const router = useRouter();
+  
+  useEffect(() => {
+    // Only redirect if NOT logged in AND loading is complete
+    if (isLoggedIn && !isLoading) {
+      router.push(`/${locale}`);
+    }
+  }, [isLoggedIn, isLoading]); // Add isLoading and locale to dependencies
+  
+
   return (
-    <div className="min-h-screen bg-gradient-to-br from-background to-background-secondary">
+    <>
+    {!isLoading && !isLoggedIn &&
+      <div className="min-h-screen bg-gradient-to-br from-background to-background-secondary">
       <LoginForm />
-    </div>
+      </div>
+    }
+    </>
   );
 }
 
