7c983ac2cad814a4693d4624e6c692dce0dfd504
sieujs <gamefun206@gmail.com>
2025-04-16 01:17:03 +0700

    add conference

--- src/hooks/dashboard/myConferences/useMyConferences.ts
+++ src/hooks/dashboard/myConferences/useMyConferences.ts
@@ -2,17 +2,19 @@
 import { useState, useEffect } from 'react';
 import { AddedConference } from '../../../models/send/addConference.send'; //  Define this type
 import { appConfig } from '@/src/middleware';
+import { ConferenceInfo } from '@/src/models/response/conference.list.response';
+import { ConferenceResponse } from '@/src/models/response/conference.response';
 
 // Define the shape of the data returned by the API
 interface UseMyConferencesResult {
-    conferences: AddedConference[];
+    conferences: ConferenceResponse[];
     isLoading: boolean;
     error: string | null;
     refetch: () => void; // Add refetch function
 }
 
 const useMyConferences = (userId: string): UseMyConferencesResult => {
-    const [conferences, setConferences] = useState<AddedConference[]>([]);
+    const [conferences, setConferences] = useState<ConferenceResponse[]>([]);
     const [isLoading, setIsLoading] = useState<boolean>(true);
     const [error, setError] = useState<string | null>(null);
 
@@ -20,12 +22,20 @@ const useMyConferences = (userId: string): UseMyConferencesResult => {
         setIsLoading(true);
         setError(null);
         try {
-            const response = await fetch(`${appConfig.NEXT_PUBLIC_DATABASE_URL}/api/v1/user/${userId}/my-conferences`);
+            const response = await fetch(`${appConfig.NEXT_PUBLIC_DATABASE_URL}/api/v1/conference/user` ,
+                {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${localStorage.getItem('token')}`,
+                    },
+                }
+            );
             if (!response.ok) {
                 throw new Error(`Failed to fetch conferences: ${response.status} ${response.statusText}`);
             }
-            const data: AddedConference[] = await response.json();
-            console.log(data);
+            const data: ConferenceResponse[] = await response.json();
+            console.log("ata" , data);
             setConferences(data);
         } catch (err: any) {
             setError(err.message || 'An unknown error occurred');
