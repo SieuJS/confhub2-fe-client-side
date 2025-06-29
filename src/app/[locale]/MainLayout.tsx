fc469005d98a38b6aa92a6564de1d420e6729efe
thang <nguyenhuuthang02032003@gmail.com>
2025-06-29 10:11:33 +0700

    feat: add poster

--- src/app/[locale]/MainLayout.tsx
+++ src/app/[locale]/MainLayout.tsx
@@ -1,10 +1,10 @@
 // src/app/[locale]/MainLayout.tsx
 
-'use client';
+'use client'
 
-import React from 'react';
-import { usePathname } from '@/src/navigation';
-import { AppPathname } from '@/src/navigation';
+import React from 'react'
+import { usePathname } from '@/src/navigation'
+import { AppPathname } from '@/src/navigation'
 
 // Danh sách các đường dẫn không thay đổi
 const PATHS_WITHOUT_HEADER: AppPathname[] = [
@@ -18,7 +18,8 @@ const PATHS_WITHOUT_HEADER: AppPathname[] = [
   '/auth/verify-email',
   '/auth/forgot-password',
   '/auth/reset-password',
-];
+  '/poster'
+]
 
 const PATHS_WITHOUT_FOOTER: AppPathname[] = [
   '/chatbot/landing',
@@ -31,21 +32,26 @@ const PATHS_WITHOUT_FOOTER: AppPathname[] = [
   '/auth/verify-email',
   '/auth/forgot-password',
   '/auth/reset-password',
-  '/dashboard'
-];
+  '/dashboard',
+  '/poster'
+]
 
 // THAY ĐỔI: Cập nhật props interface
 interface MainLayoutProps {
-  children: React.ReactNode;
-  header: React.ReactNode; // Prop để nhận Header đã được render
-  footer: React.ReactNode; // Prop để nhận Footer đã được render
+  children: React.ReactNode
+  header: React.ReactNode // Prop để nhận Header đã được render
+  footer: React.ReactNode // Prop để nhận Footer đã được render
 }
 
-export default function MainLayout({ children, header, footer }: MainLayoutProps) {
-  const pathname = usePathname();
+export default function MainLayout({
+  children,
+  header,
+  footer
+}: MainLayoutProps) {
+  const pathname = usePathname()
 
-  const showHeader = !PATHS_WITHOUT_HEADER.includes(pathname as AppPathname);
-  const showFooter = !PATHS_WITHOUT_FOOTER.includes(pathname as AppPathname);
+  const showHeader = !PATHS_WITHOUT_HEADER.includes(pathname as AppPathname)
+  const showFooter = !PATHS_WITHOUT_FOOTER.includes(pathname as AppPathname)
 
   return (
     <>
@@ -59,5 +65,5 @@ export default function MainLayout({ children, header, footer }: MainLayoutProps
       {/* THAY ĐỔI: Render prop `footer` thay vì import và render component */}
       {showFooter && footer}
     </>
-  );
+  )
 }
---
+++ src/app/[locale]/poster/page.tsx
@@ -0,0 +1,12 @@
+// app/poster/page.tsx
+import Poster from './Poster'
+import PosterExporter from './PosterExporter'
+
+export default function PosterPage() {
+  return (
+    <main>
+      <Poster />
+      {/* <PosterExporter /> */}
+    </main>
+  )
+}
